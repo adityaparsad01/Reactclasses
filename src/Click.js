a9f3733c432a972cfa198e31ee11ad126efa0531
unknown <adityaparsad01@gmail.com>
2020-05-13 18:45:21 +0530

    first commit

--- src/Click.js
+++ src/Click.js
@@ -15,9 +15,9 @@ export class click extends Component {
 
     componentDidMount()
     {
-        fetch('https://script.googleusercontent.com/macros/echo?user_content_key=YDV9MmjQYfG36MgdU8fIY8j1HZplHxZM7eQtPB3HO46zR9bONhcNTk_eEMfCQWUAhBbnKrtsi9NupLCWk8QSVJPkrmBEyWa-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHMclrOu3rJUFqJwh4WY4laoMS6zkwtVBNTHGN9eZ5WZsAstNoXAZ7jW4ojdmDAfv1SsFbGVI4aT&lib=MZoW5iqVNUhx3yRKbHDiitILi0zpk96DG').then((response) =>{
+        fetch('https://script.google.com/macros/s/AKfycbxMRQFQYTj2qQd1YlM4NoWqn4JO9hXOJxrR01IPYla3xNsnseMx/exec').then((response) =>{
             response.json().then((result) => {
-                console.log(result);
+                // console.log(result);
                 this.setState({users:result.Student})
             })
         })
