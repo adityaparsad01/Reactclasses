db2323e7c39392107bdc8a74c21b50ec821d944c
unknown <adityaparsad01@gmail.com>
2020-05-14 12:35:57 +0530

    change has been added

--- src/Click.js
+++ src/Click.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 export class click extends Component {
-<<<<<<< HEAD
   constructor() {
     super();
     this.state = {
@@ -32,51 +31,6 @@ export class click extends Component {
       </div>
     );
   }
-=======
-
-    constructor()
-    {
-        super()
-        this.state = {
-            users:null
-        }
-    }
-
-    componentDidMount()
-    {
-        fetch('https://script.google.com/macros/s/AKfycbxMRQFQYTj2qQd1YlM4NoWqn4JO9hXOJxrR01IPYla3xNsnseMx/exec').then((response) =>{
-            response.json().then((result) => {
-                // console.log(result);
-                this.setState({users:result.Student})
-            })
-        })
-    }
-
-    
-
-    render() {
-        return (
-            <div>
-               {
-                   this.state.users ?
-                   this.state.users.map((item,i) =>
-
-                   <div className="card" key={i}>
-                       
-                       <h1>{item.Name}</h1>
-                       <p>{item.Subject}</p>
-                       <p>{item.Course}</p>
-                       <p>{item.Year}</p>
-                   </div>
-                   
-                   )
-                   :
-                   null
-               }
-            </div>
-        )
-    }
->>>>>>> 483fd328369f2fb7c87fee26ecf661e1de601b4d
 }
 
 export default click;
